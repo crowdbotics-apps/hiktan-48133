27ce7f307d65d96dce553e07c7eac6516c69bc92
Crowdbotics <team@crowdbotics.com>
2024-05-28 01:03:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hiktan-48133",
+  url: "https://hiktan-48133.botics.co",
+  api: "https://hiktan-48133.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
